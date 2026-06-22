b4aedaa9e2495b3d1cfecf6fdcc1f6c006660ae3
Rajdeep Singh <radewala.2@gmail.com>
2026-06-22 18:43:55 +0530

    remove the alpine collapse package

--- assets/js/alpine.js
+++ assets/js/alpine.js
@@ -1,10 +1,9 @@
 // Initialize Alpine.js
-import Alpine from 'alpinejs';
+import Alpine from 'alpinejs'; 
 
 export function initAlpine() {
   // Make Alpine available globally for use in HTML
   window.Alpine = Alpine;
-  
   // Start Alpine
   Alpine.start();
 }
